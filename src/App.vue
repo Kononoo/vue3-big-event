48d0aed63d8971d84c6312e814090d1d671fd058
ronan <321393295@qq.com>
2023-11-23 17:39:15 +0800

    pinia独立维护和仓库统一导出

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,8 @@
 // 获取路由对象 router  const router = useRouter()
 // 获取路由参数 route   const route = useRoute()
 import { useRouter, useRoute } from 'vue-router'
-import { useUserStore } from '@/stores/user'
+import { useUserStore } from '@/stores/modules/user'
+import { useCounterStore } from '@/stores/modules/counter'
 
 const router = useRouter()
 const route = useRoute()
@@ -12,25 +13,27 @@ const goList = () => {
   console.log(route)
 }
 const userStore = useUserStore()
+const countStore = useCounterStore()
 </script>
 
 <template>
   <h2>I am App</h2>
   <div>
-    <el-button type="danger" @click="router.push('/home')"
-      >跳转到首页</el-button
-    >
+    <el-button type="danger" @click="router.push('/home')">跳转到首页</el-button>
     <el-button type="danger" @click="router.go(1)">跳转到表业</el-button>
     <el-button type="danger" @click="goList">跟我走吧</el-button>
 
     <p>{{ userStore.token }}</p>
-    <el-button type="success" round @click="userStore.setToken('hello')"
-      >登录</el-button
-    >
-    <el-button type="success" round @click="userStore.removeToken"
-      >退出登录</el-button
-    >
+    <el-button type="success" round @click="userStore.setToken('hello')">登录 </el-button>
+    <el-button type="success" round @click="userStore.removeToken">退出登录 </el-button>
+    <hr />
+    <p>{{ countStore.count }} - {{ countStore.doubleCount }}</p>
+    <el-button type="primary" round @click="countStore.increment">点击我加加</el-button>
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+* {
+  margin: 10px;
+}
+</style>
