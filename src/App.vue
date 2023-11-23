88b2c228cfd7dbe3696f0283af9d640d2720cf7e
ronan <321393295@qq.com>
2023-11-23 17:20:47 +0800

    配置pinia仓库&持久化

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
 // 获取路由对象 router  const router = useRouter()
 // 获取路由参数 route   const route = useRoute()
 import { useRouter, useRoute } from 'vue-router'
+import { useUserStore } from '@/stores/user'
 
 const router = useRouter()
 const route = useRoute()
@@ -10,6 +11,7 @@ const goList = () => {
   console.log(router)
   console.log(route)
 }
+const userStore = useUserStore()
 </script>
 
 <template>
@@ -20,100 +22,15 @@ const goList = () => {
     >
     <el-button type="danger" @click="router.go(1)">跳转到表业</el-button>
     <el-button type="danger" @click="goList">跟我走吧</el-button>
+
+    <p>{{ userStore.token }}</p>
+    <el-button type="success" round @click="userStore.setToken('hello')"
+      >登录</el-button
+    >
+    <el-button type="success" round @click="userStore.removeToken"
+      >退出登录</el-button
+    >
   </div>
 </template>
 
 <style scoped></style>
-
-<!--
-<script setup>
-import { RouterLink, RouterView } from 'vue-router'
-</script>
-
-<template>
-  <header>
-    <img
-      alt="Vue logo"
-      class="logo"
-      src="@/assets/logo.png"
-      width="125"
-      height="125"
-    />
-
-    <div class="wrapper">
-      &lt;!&ndash; <HelloWorld msg="You did it!" /> &ndash;&gt;
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
-</template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(&#45;&#45;color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(&#45;&#45;color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(&#45;&#45;section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
--->
--- src/main.js
+++ src/main.js
@@ -1,13 +1,13 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
 import App from './App.vue'
 import router from './router'
 import '@/assets/main.scss'
+import persist from 'pinia-plugin-persistedstate'
 
 const app = createApp(App)
 
-app.use(createPinia())
+app.use(createPinia().use(persist))
 app.use(router)
 
 app.mount('#app')
